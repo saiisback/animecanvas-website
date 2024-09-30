eb2d20387890c23045f07cf2345b4c73662ac2e0
sai karthik ketha <69042752+saiisback@users.noreply.github.com>
2024-09-30 16:18:47 +0000

    update 0.2

---
+++ app/listing/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function listingPage() {
+  return (
+    <div>listingPage</div>
+  )
+}
+
+export default listingPage
--- components/navbar.tsx
+++ components/navbar.tsx
@@ -1,9 +1,28 @@
 import React from 'react'
+import { FcFlashOn } from "react-icons/fc";
+import { FcShop } from "react-icons/fc";
+
+
+import Link from "next/link";
 
 function Navbar() {
   return (
-    <div className='font-bold flex'> <h1>hello</h1>
-    <p>i am ved</p></div>
+    <div className='w-full text-black m-3'>
+      <nav className='flex justify-between'>
+        <div className='flex justify-evenly'>
+          <Link href="#" className='flex items-center align-middle'>New Drops <FcFlashOn /></Link>
+          <Link href="#" className='flex items-center align-middle'>Catlog <FcShop/></Link>
+          <Link href="#" className='flex items-center align-middle'>Policies</Link>
+        </div>
+        <div>
+          <h1>ANIME CANVAS</h1>
+        </div>
+        <div className='flex'>
+          <Link href="#" className='flex items-center align-middle'>Search</Link>
+          <Link href="#" className='flex items-center align-middle'>Cart</Link>
+        </div>
+      </nav>
+    </div>
   )
 }
 
